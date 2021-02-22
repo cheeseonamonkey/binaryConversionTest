11fcfcfaf4927d3b8c8d4d7b4c8242eea1df52a5
Alexander poop <cheeseonamonkey@gmail.com>
2021-02-21 17:19:21 -0700

    Add project files.

---
+++ binaryConversionTest/ByteConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace binaryConversionTest
+{
+    public static class ByteConverter
+    {
+        //takes a binary string, int, or byte and converts to hex
+        public static string ByteToHex(string binaryString)
+        {
+            sbyte sbite = Convert.ToSByte(binaryString, 2);
+            return ByteToHex(sbite);
+        }
+        public static string ByteToHex(sbyte sbite)
+        {
+            return ByteToHex((byte)sbite);
+        }
+        public static string ByteToHex(byte bite)
+        {
+            string hex = bite.ToString("X");
+            return hex;
+        }
+
+        //takes hex as a string or int and converts to a binary string
+        public static string HexToByte(string hex)
+        {
+            sbyte bite = Convert.ToSByte(hex, 16);
+            string bin = Convert.ToString((byte)bite, 2);
+
+            //adds trailing zeros
+            int n = 8 - bin.Length;
+            if (n > 0)
+                for (int i = 0; i < n; i++)
+                    bin = $"0{bin}";
+
+            return bin;
+        }
+        public static string HexToByte(int hex)
+        {
+            return HexToByte(hex.ToString());
+        }
+    }
+}
---
+++ binaryConversionTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace binaryConversionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{ByteConverter.ByteToHex("10010000")}");
+            Console.WriteLine($"\n");
+            Console.WriteLine($"{ByteConverter.HexToByte("90")}");
+
+
+
+        }
+    }
+}
